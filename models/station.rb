f235b9952f40717d9a205588e96335472be2bc15
Ian Young <ian.greenleaf@gmail.com>
2013-11-19 22:20:06 -0800

    View for percent overcast, woot!
    Map reduce mothafucka!

--- models/station.rb
+++ models/station.rb
@@ -16,4 +16,32 @@ class Station < CouchRest::Model::Base
   property :daily, array: true, default: (365.times.map {{}}) do |month|
     month.property :overcast_percentages, Array
   end
+
+  design do
+    view :overcast_m_h,
+      map:
+        'function(doc) {
+          if (doc.daily) {
+            doc.daily.forEach(function(day, dayNum) {
+              var month = (new Date(2001, 0, dayNum)).getMonth() + 1;
+              if (day.overcast_percentages) {
+                day.overcast_percentages.forEach(function(pct, hour) {
+                  var period = hour < 8 || hour >= 19 ? "night" : "day";
+                  emit([doc._id, period, month, hour], [pct, 1]);
+                });
+              }
+            });
+          }
+        }',
+      reduce:
+        'function(keys, values, rereduce) {
+          var sum = 0.0;
+          var hours = 0.0;
+          values.forEach(function(arr) {
+            sum += arr[0];
+            hours += arr[1];
+          });
+          return [sum, hours];
+        }'
+  end
 end
